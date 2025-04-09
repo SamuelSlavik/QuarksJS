ff4e8e96cb908778cd7e632bbfbd6bada4bbd482
samuel <samuel.slavik@protonmail.com>
2025-04-09 14:32:53 +0200

    Add storybook

---
+++ packages/layout/src/components/navigation/__tests__/Navigation.stories.tsx
@@ -0,0 +1,17 @@
+import { Navigation } from '../Navigation';
+import { StoryObj } from '@storybook/react';
+
+const meta = {
+    title: 'Layout/Navigation',
+    component: Navigation,
+    args: {
+        logo: <div>Logo</div>,
+        menu: 'menu',
+        content: 'a',
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
